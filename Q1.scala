856e6ac0dcc776f571713ad92cc6ddf510948b8f
rathinimal <rathinarasanimal@gmail.com>
2024-06-24 17:40:47 +0530

    files uploaded

---
+++ Q1.scala
@@ -0,0 +1,20 @@
+object expression{
+    def main(args: Array[String]): Unit = {
+        var  k , i , j :Int= 2;
+        var m,n :Int = 5;
+        var f : Float = 12.0f
+        var g : Float = 4.0f
+        var c = 'X'
+
+        println(k+ 12 * m)
+        println(m/j)
+        println(n%j)
+        println(m/j*j)
+        println(f+10.5 + g)
+        i = i+1 // for ++i*n
+        println(i)
+        println(i*n)
+
+
+    }
+}
---
+++ Q2.scala
@@ -0,0 +1,36 @@
+object expression2{
+    def main(args: Array[String]): Unit = {
+        var a ,b,c,d :Int = 0
+        a=2
+        b=3
+        c=4
+        d=5
+
+        var k : Float = 4.3f
+
+        b = b-1 // for --b
+        println(b)
+        println(b*a+c*d)
+
+        d = d-1 // for d--
+    
+        println(a)
+        a = a+1
+
+        var g = 0
+        println(-2*(g-k)+c)
+
+        
+        println(c)
+        c= c+1
+
+        c=c+1
+        c= c*a
+        println(c)
+        println(c*a)
+        a = a+1 // a++
+        println(a)
+
+
+    }
+}
